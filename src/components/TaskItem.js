d9cf5e0b4c449effb22bc1feb9593f265463fd6d
Doni Ramos da Silva <doni.silva@goias.gov.br>
2025-11-24 15:56:56 -0300

    feat: add TaskItem component

---
+++ src/components/TaskItem.js
@@ -0,0 +1,11 @@
+const TaskItem = ({task}) => {
+    return (
+        <>
+        <h1> {task.description} </h1>
+        <p> {task.isCompleted ? "Completa" : "Incompleta"}</p>
+
+        </>
+    );
+}
+
+export default TaskItem;
