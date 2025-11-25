14fb30a9675490b7b4264f96860c40993b300a3e
DoniDevRs <doni.devrs@gmail.com>
2025-11-25 20:23:38 -0300

    feat: add Sidebar component.

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,23 @@
+import CustomButton from './CustomButton';
+
+import './SideBar.scss'
+import logo from '../assets/images/logo.png'
+
+const SideBar = () => {
+    return (
+        <div className="sidebar-container">
+            <div className="logo">
+                <img src={logo} alt="react" />
+
+            </div>
+
+            <div className="sign-out">
+                <CustomButton>Sair</CustomButton>
+            </div>
+
+        </div>
+    )
+
+}
+
+export default SideBar;
