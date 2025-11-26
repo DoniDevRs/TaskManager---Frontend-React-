be3679daba01753ba837570ca295fc2f35ea0f93
Doni Ramos da Silva <doni.silva@goias.gov.br>
2025-11-26 08:17:17 -0300

    feat: add react-router-dom setup and hoem page.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter } from 'react-router-dom'
 
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
@@ -11,8 +12,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <ToastContainer />
+    <BrowserRouter>
+      <App />
+      <ToastContainer />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
